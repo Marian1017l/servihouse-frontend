8f2f89fb4c6cc23c7b2c5d461ac3850b265d915a
Marian1017l <mariana.lopez35806@ucaldas.edu.co>
2025-04-30 19:44:25 -0500

    feat created initial layout of header

--- src/components/dashboard/homepage/Header.jsx
+++ src/components/dashboard/homepage/Header.jsx
@@ -2,20 +2,22 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return (
-    <header className="header">
-      <div className="header-content">
-        <h1>ServiHouse</h1>
-        <nav>
-          <ul className="nav-links">
-            <li><a href="/">Inicio</a></li>
-            <li><a href="/about">Acerca</a></li>
-            <li><a href="/contact">Contacto</a></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  );
+    return (
+        <header className="header">
+            <div className="header-content">
+                <div className="logo">
+                    <h1>ServiHouse</h1>
+                </div>
+                <nav>
+                    <ul className="nav-links">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/login">Log in</a></li>
+                        <li><a href="/signup" className="signup-btn">Sign up</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
