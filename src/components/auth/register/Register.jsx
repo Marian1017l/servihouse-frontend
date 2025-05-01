dfce12ba2f1e9b2b8989d00537ed43a168808f8f
Marian1017l <mariana.lopez35806@ucaldas.edu.co>
2025-04-30 21:36:55 -0500

    feat: created signup interface layout

--- src/components/auth/register/Register.jsx
+++ src/components/auth/register/Register.jsx
@@ -3,24 +3,23 @@ import './Register.css';
 
 const SignUp = () => {
 
-    return (
-        <div>
-          <h1>Ini</h1>
-          <form>
-            <label>
-              Usuario:
-              <input type="text" name="username" />
-            </label>
-            <br />
-            <label>
-              Contraseña:
-              <input type="password" name="password" />
-            </label>
-            <br />
-            <button type="submit">Ingresar</button>
-          </form>
+  return (
+    <div className="register-page">
+      <div className="register-container">
+        <h2 className="register-title">CREATE A NEW ACCOUNT</h2>
+        <form className="register-form">
+          <input type="name" placeholder="Name" required />
+          <input type="email" placeholder="Email" required />
+          <input type="password" placeholder="Password" required />
+          <input type="number" placeholder="Phone" required />
+          <button type="submit">CONTINUE</button>
+        </form>
+        <div className="register-links">
+          <a href="/auth/login">Log In</a>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;
